ede586ee7de0dd757bb313695a1c1c21fef19ed4
Hamon <hamv12@uqo.ca>
2024-08-14 19:59:36 -0400

    trouvé idées de mes 20 requests

--- src/ReqetesSQL.sql
+++ src/ReqetesSQL.sql
@@ -34,5 +34,84 @@ ORDER BY nom_producteur
 -- Request #4 
 -- les noms des distributeurs et des producteur qui ont fait des transactions.
 
+
+
 -- Request #5 
 -- les noms des producteurs qui ont fait affaire avec des distributeurs.
+
+
+
+-- Request #6 
+-- Les vins du Quebec
+
+
+
+-- Request #7
+-- compte de vins par type
+
+
+
+-- Request #8
+-- cout moyen des transactions
+
+
+
+-- Request #9
+-- nombre total de transactions pour un producteur
+
+
+
+-- Request #10
+-- top 5 des vins les plus vieux
+
+
+
+-- Request #11
+-- top 10 des transactions les plus récentes
+
+
+
+-- Request #12
+-- top 10 des transactions les plus payantes
+
+
+
+-- Request #13
+-- nombre total de transactions pour un vin spécifique
+
+
+
+-- Request #14
+-- nombre total de transactions de tous les vendeurs
+
+
+
+-- Request #15
+-- transaction détaillé avec nom des producteurs et nom vin
+
+
+
+-- Request #16
+-- trouver le vin le plus profitable de toutes les transactions
+
+
+
+-- Request #17
+-- top 3 des producteurs avec les plus de transactions
+
+
+
+-- Request #18
+-- les vins qui n'ont pas été vendu
+
+
+
+-- Request #19
+-- le distributeur qui a le plus grand nombre de transaction en moyenne
+
+
+
+-- Request #20
+-- Analyser les tendances de vente mensuelles pour un vin spécifique
+
+
