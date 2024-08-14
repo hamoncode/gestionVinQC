7eab98b864fe5a100ce61ba805d83863ea5e67b9
Hamon <hamv12@uqo.ca>
2024-08-14 16:59:54 -0400

    data bug found

---
+++ src/ReqetesSQL.sql
@@ -0,0 +1,32 @@
+USE BD_GestionVinsQC
+
+-- Request #1
+-- tester si les données ont bien été entrées
+
+Select *
+From Vin
+
+Select *
+From Producteur
+
+Select *
+From Detaillant
+
+Select *
+From Distributeur
+
+Select *
+From Transactions
+
+-- Request #2 
+-- tous les vin et nom producteur
+
+
+-- Request #3 
+-- les noms des distributeurs et des détaillants qui ont fait des transactions.
+
+-- Request #4 
+-- les noms des distributeurs et des producteur qui ont fait des transactions.
+
+-- Request #5 
+-- les noms des producteurs qui ont fait affaire avec des distributeurs.
