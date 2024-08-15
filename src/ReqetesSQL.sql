0711442e34830c9c964a27db9827408b9a0c4ff0
Hamon <hamv12@uqo.ca>
2024-08-15 12:38:56 -0400

    requetes SQL fini

--- src/ReqetesSQL.sql
+++ src/ReqetesSQL.sql
@@ -66,76 +66,125 @@ GROUP BY v.nom_vin
 ORDER BY nombre_transactions DESC
 
 -- Requête #6 
--- Les vins du Quebec
+-- Les vins du Quebec les plus vieux
 
+SELECT v.nom_vin , v.millesime_vin
+FROM Vin v
+JOIN Producteur p ON v.id_producteur = p.id_producteur
+WHERE region_producteur = 'QC'
 
 
 -- Requête #7
 -- compte de vins par type
 
+SELECT type_vin , COUNT(*) compte_de_vin
+FROM Vin
+GROUP BY type_vin
 
 
 -- Requête #8
 -- cout moyen des transactions
 
-
+SELECT AVG(Prix) AS moyenne_prix
+FROM Transactions 
 
 -- Requête #9
 -- nombre total de transactions pour un producteur
 
-
+SELECT p.nom_producteur , COUNT(t.id_transactions) AS nombre_transactions
+FROM Transactions t
+JOIN Producteur p ON t.id_producteur = p.id_producteur
+GROUP BY nom_producteur
 
 -- Requête #10
--- top 5 des vins les plus vieux
+-- top 5 des vins les plus vieux 
 
-
+SELECT TOP 5 v.nom_vin , v.millesime_vin
+FROM Vin v
+ORDER BY millesime_vin 
 
 -- Requête #11
 -- top 10 des transactions les plus récentes
 
-
+SELECT TOP 10 id_transactions, DATEDIFF(DAY, date, GETDATE()) AS difference_jour
+FROM Transactions
+ORDER BY date DESC;
 
 -- Requête #12
 -- top 10 des transactions les plus payantes
 
-
+SELECT TOP 10 id_transactions, PRIX 
+FROM Transactions
+ORDER BY PRIX DESC
 
 -- Requête #13
 -- nombre total de transactions pour un vin spécifique
 
-
+SELECT v.nom_vin, t.Prix, COUNT(t.id_transactions) AS nombre_transaction
+FROM Transactions t
+JOIN Vin v ON v.id_vin = t.id_vin
+GROUP BY v.nom_vin, t.Prix
+ORDER BY nombre_transaction DESC;
 
 -- Requête #14
 -- nombre total de transactions de tous les vendeurs
 
+SELECT id_producteur, id_distributeur, COUNT(id_transactions) AS nombre_transaction
+FROM Transactions 
+GROUP BY id_producteur, id_distributeur
+ORDER BY nombre_transaction DESC;
 
 
 -- Requête #15
 -- transaction détaillé avec nom des producteurs et nom vin
 
-
+SELECT p.nom_producteur , p.adresse_producteur , p.region_producteur, v.nom_vin, v.type_vin, v.millesime_vin
+FROM PRODUCTEUR p
+JOIN Vin v ON v.id_producteur = p.id_producteur
+ORDER BY nom_producteur
 
 -- Requête #16
 -- trouver le vin le plus profitable de toutes les transactions
 
-
+SELECT DISTINCT TOP 1 v.nom_vin , SUM(t.prix) AS somme_transactions
+FROM Transactions t
+JOIN VIN v ON v.id_vin = t.id_vin
+GROUP BY v.nom_vin
+ORDER BY somme_transactions DESC
 
 -- Requête #17
--- top 3 des producteurs avec les plus de transactions
+-- top 3 des producteurs avec le plus de transactions
 
-
+SELECT TOP 3 p.nom_producteur , COUNT(id_transactions) AS nombre_transactions
+FROM Transactions t
+JOIN Producteur p ON p.id_producteur = t.id_producteur
+GROUP BY nom_producteur
+ORDER BY nombre_transactions DESC
 
 -- Requête #18
 -- les vins qui n'ont pas été vendu
 
-
+SELECT v.nom_vin
+FROM Vin v
+LEFT JOIN Transactions t ON v.id_vin = t.id_vin
+WHERE t.id_vin IS NULL;
 
 -- Requête #19
--- le distributeur qui a le plus grand nombre de transaction en moyenne
+-- le distributeur qui a le plus grand nombre de transaction 
 
+SELECT d.nom_distributeur , COUNT(t.id_transactions) AS nombre_transaction 
+FROM Transactions t 
+JOIN Distributeur d ON d.id_distributeur = t.id_distributeur
+GROUP BY nom_distributeur
 
 
 -- Requête #20
--- Analyser les tendances de vente mensuelles pour un vin spécifique
+-- vente mensuelles pour un vin spécifique
+
+SELECT FORMAT(t.date, 'yyyy-MM') AS mois, SUM(t.Prix) AS vente_du_mois
+FROM Transactions t
+WHERE t.id_vin = 48
+GROUP BY FORMAT(t.date, 'yyyy-MM')
+ORDER BY mois
 
 
