5eadd761428af02f80f7abc6d2cb0e5ac36bc2d5
Hamon <hamv12@uqo.ca>
2024-08-14 20:49:30 -0400

    utilisation francais

--- src/ReqetesSQL.sql
+++ src/ReqetesSQL.sql
@@ -19,12 +19,12 @@ Select *
 From Transactions
 
 
--- REQUEST #1
+-- Requête #1
 -- producteur qui on vendu plus que 5 types de vins
 
 
 
--- Request #2 
+-- Requête #2 
 -- afficher le nom des vins selon leurs producteurs en ordre alphabétique
 
 SELECT nom_vin, nom_producteur
@@ -32,7 +32,7 @@ FROM Vin v,Producteur p
 WHERE v.id_producteur = p.id_producteur
 ORDER BY nom_producteur
 
--- Request #3
+-- Requête #3
 -- les noms des distributeurs et des détaillants qui ont fait des transactions.
 
 Select di.nom_distributeur , de.nom_detaillant, t.date, t.Prix
@@ -40,87 +40,87 @@ From Transactions t , Distributeur di , Detaillant de
 Where t.id_distributeur = di.id_distributeur
 And t.id_detaillant = de.id_detaillant
 
--- Request #4 
+-- Requête #4 
 -- pour le mois de juin seulement, lister les transactions de la SAQ 
 
 
 
--- Request #5 
+-- Requête #5 
 -- identifier le vin le plus populaire par le nombre de transactions
 
 
 
--- Request #6 
+-- Requête #6 
 -- Les vins du Quebec
 
 
 
--- Request #7
+-- Requête #7
 -- compte de vins par type
 
 
 
--- Request #8
+-- Requête #8
 -- cout moyen des transactions
 
 
 
--- Request #9
+-- Requête #9
 -- nombre total de transactions pour un producteur
 
 
 
--- Request #10
+-- Requête #10
 -- top 5 des vins les plus vieux
 
 
 
--- Request #11
+-- Requête #11
 -- top 10 des transactions les plus récentes
 
 
 
--- Request #12
+-- Requête #12
 -- top 10 des transactions les plus payantes
 
 
 
--- Request #13
+-- Requête #13
 -- nombre total de transactions pour un vin spécifique
 
 
 
--- Request #14
+-- Requête #14
 -- nombre total de transactions de tous les vendeurs
 
 
 
--- Request #15
+-- Requête #15
 -- transaction détaillé avec nom des producteurs et nom vin
 
 
 
--- Request #16
+-- Requête #16
 -- trouver le vin le plus profitable de toutes les transactions
 
 
 
--- Request #17
+-- Requête #17
 -- top 3 des producteurs avec les plus de transactions
 
 
 
--- Request #18
+-- Requête #18
 -- les vins qui n'ont pas été vendu
 
 
 
--- Request #19
+-- Requête #19
 -- le distributeur qui a le plus grand nombre de transaction en moyenne
 
 
 
--- Request #20
+-- Requête #20
 -- Analyser les tendances de vente mensuelles pour un vin spécifique
 
 
